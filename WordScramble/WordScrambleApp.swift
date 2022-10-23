f285dee9a87019f7a7834df089c14d3e3d4eaef1
Matthew Dolan <matthew.dolan@ballysinteractive.com>
2022-10-23 07:45:28 -0700

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Matthew Dolan on 2022-10-23.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
